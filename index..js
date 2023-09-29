f20694093fce4345510df31891236d837d74fe0f
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-09-29 14:40:27 +0530

    babel setup

---
+++ index..js
@@ -0,0 +1,8 @@
+const $43d7963e56408b24$var$pel = document.createElement("h1");
+const $43d7963e56408b24$var$text = document.createTextNode("ritik list");
+$43d7963e56408b24$var$pel.appendChild($43d7963e56408b24$var$text);
+const $43d7963e56408b24$var$root = document.getElementById("root");
+$43d7963e56408b24$var$root.appendChild($43d7963e56408b24$var$pel);
+
+
+//# sourceMappingURL=index..js.map
