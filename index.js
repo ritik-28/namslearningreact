e04e563f2ed0516cb7a8770f5ff07da2c699e5c7
ritik-28 <tiwari.ritik.1.108@gmail.com>
2023-09-29 12:52:01 +0530

    starting nam react

---
+++ index.js
@@ -0,0 +1,5 @@
+const pel = document.createElement("h1");
+const text = document.createTextNode("ritik list");
+pel.appendChild(text);
+const root = document.getElementById("root");
+root.appendChild(pel);
